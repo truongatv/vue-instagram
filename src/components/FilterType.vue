36bd775d84a4896b4f88bd59b8eaa2b2988de7a0
truongatv <truongatv2211@gmail.com>
2018-12-18 15:52:25 +0900

    done upload image

---
+++ src/components/FilterType.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="filter-type">
+      <p>{{filter.name}}</p>
+      <div class="img" :class="filter.name" :style="{ backgroundImage: 'url(' + image + ')' }" @click="selectedFilter">
+
+      </div>
+    </div>
+</template>
+
+<script>
+  import EventBus from "../event-bus.js";
+  export default {
+    name: 'FilterType',
+    props: {
+      filter: Object,
+      image: String
+    },
+    methods: {
+      selectedFilter(){
+        EventBus.$emit(
+          "filter-selected", {filter: this.filter.name}
+        )
+      }
+    }
+  }
+</script>
+
+<style lang="scss" src="../styles/filter-type.scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,95 @@
+<script src="../../../vue-stagram/src/main.js"></script>
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="app-phone">
+      <div class="phone-header">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/vue_gram_logo_cp.png" />
+        <a class="cancel-cta" v-if="step === 2 || step === 3" @click="goToHome">Cancel</a>
+        <a class="next-cta" v-if="step === 2" @click="step++">Next</a>
+        <a class="next-cta" v-if="step === 3" @click="sharePost">Share</a>
+      </div>
+      <phone-body
+        :step = "step"
+        :posts = "posts"
+        :filters = "filters"
+        :image="image"
+        :selectedFilter="selectedFilter"
+        v-model = "caption"
+      />
+      <div class="phone-footer">
+        <div class="home-cta">
+          <i class="fas fa-home fa-lg" @click="goToHome"></i>
+        </div>
+        <div class="upload-cta">
+          <input type="file" name="file" id="file" class="inputfile" @change="uploadImage" :disabled = "step !== 1" />
+          <label for="file">
+            <i class="far fa-plus-square fa-lg"></i>
+          </label>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import PhoneBody from "../components/PhoneBody"
+import posts from "../data/posts";
+import filters from "../data/filters";
+import EventBus from "../event-bus.js";
 
 export default {
-  name: 'home',
+  name: "App",
   components: {
-    HelloWorld
+    "phone-body": PhoneBody
+  },
+  data() {
+    return {
+      step: 1,
+      posts,
+      filters,
+      image: "",
+      selectedFilter: "",
+      caption: ""
+    };
+  },
+  methods: {
+    uploadImage(evt) {
+      const files = evt.target.files;
+      if (!files.length) return;
+      const reader = new FileReader();
+      reader.readAsDataURL(files[0]);
+      reader.onload = evt => {
+        this.image = evt.target.result;
+        this.step = 2;
+      };
+      document.querySelector("#file").value = "";
+    },
+    goToHome(){
+      this.image = "";
+      this.selectedFilter = "";
+      this.caption = "";
+      this.step = 1;
+    },
+    sharePost() {
+      const post = {
+        username: "vuegram",
+        userImage: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/vue_lg_bg.png",
+        postImage: this.image,
+        likes: 0,
+        caption: this.caption,
+        filter: this.selectedFilter
+      };
+      this.posts.unshift(post);
+      this.goToHome();
+    }
+  },
+  created() {
+    EventBus.$on("filter-selected", evt=> {
+      this.selectedFilter = evt.filter;
+    })
   }
 }
 </script>
+<style lang="scss" src="../styles/app.scss">
+
+</style>
